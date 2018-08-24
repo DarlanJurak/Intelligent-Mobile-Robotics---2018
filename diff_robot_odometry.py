a49d5f15afb09affe1c65beccced08a4bc34ee62
DarlanJurak <darlan.jurak@gmail.com>
2018-08-24 11:12:12 -0300

    DarlanJurak

--- diff_robot_odometry.py
+++ diff_robot_odometry.py
@@ -1,7 +1,12 @@
 print "***** Robot Design Constants Definition *****"
-x_linear_vel            =   input('Please, insert x linear velocity                     (m/s): ')
-z_angular_ang           =   input('Please, insert z angular velocity                    (rad): ')
-distance_btwn_wheels    =   input('Please, insert the distance between the robot wheels (m)  : ')
+distance_btwn_wheels            =   input('Please, insert the distance between the robot wheels (m)     : ')
+encoder_ticks_per_revolution    =   input('Please, insert the encoder`s number of ticks per revolution  : ')
+wheel_radius                    =   input('Please, insert the robot`s wheel radius (m)                  : ')
+print "***** Current Robot Position *****"
+robot_current_x_pos             =   input('Please, insert robot current X position  :')
+robot_current_y_pos             =   input('Please, insert robot current Y position  :')
+robot_current_angle             =   input('Please, insert robot current angle (rad) :')
+
 
 velocity_left           =   2*x_linear_vel - z_angular_ang*distance_btwn_wheels
 velocity_right          =   2*x_linear_vel + z_angular_ang*distance_btwn_wheels
